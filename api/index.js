32d7efba76f689554dd312da1d0703e92c083ff5
Janaki4 <janakiraman781@gmail.com>
2023-11-13 20:10:44 +0530

    API

---
+++ api/index.js
@@ -0,0 +1,56 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
+import path from 'path';
+import cors from 'cors';
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const __dirname = path.resolve();
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
+var corsOptions = {
+  origin: ["http://localhost:3000","http://localhost:3001"] 
+};
+
+app.use(cors(corsOptions));
+app.use(cors())
+app.options('*',cors())
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.listen(6060, () => {
+  console.log('Server listening on port 6060');
+});
+
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
+});
